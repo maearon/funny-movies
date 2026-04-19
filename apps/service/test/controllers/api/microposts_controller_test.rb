de2eb4eac05d0583af79bd234cf3f741ec202c29
maearon <manhng132@gmail.com>
2026-04-19 16:05:07 +0700

    TEST FIXTURES update: add tests for micropost creation and authorization handling

--- apps/service/test/controllers/api/microposts_controller_test.rb
+++ apps/service/test/controllers/api/microposts_controller_test.rb
@@ -1,13 +1,39 @@
 require "test_helper"
 
 class Api::MicropostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_microposts_index_url
-    assert_response :success
+  setup do
+    @user = users(:one)
+    @token = Jwt::User::EncodeTokenService.call(@user.id).first
   end
 
-  test "should get create" do
-    get api_microposts_create_url
+  test "create micropost requires authentication" do
+    assert_no_difference "Micropost.count" do
+      post api_microposts_url,
+           params: {
+             micropost: {
+               content: "https://www.youtube.com/watch?v=pRdv7lDoqIo",
+               title: "Test",
+               youtube_id: "pRdv7lDoqIo"
+             }
+           },
+           as: :json
+    end
+    assert_response :unauthorized
+  end
+
+  test "create micropost enqueues notify job when authorized" do
+    assert_enqueued_jobs 1, only: NotifyUsersJob do
+      post api_microposts_url,
+           params: {
+             micropost: {
+               content: "https://www.youtube.com/watch?v=jNQXAC9IVRw",
+               title: "Me at the zoo",
+               youtube_id: "jNQXAC9IVRw"
+             }
+           },
+           headers: { "Authorization" => "Bearer #{@token}" },
+           as: :json
+    end
     assert_response :success
   end
 end
