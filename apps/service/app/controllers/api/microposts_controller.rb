d5c7d214d31e5596b3f06cf5714411d68f80ef1e
maearon <manhng132@gmail.com>
2026-04-19 15:34:11 +0700

    WEBSOCKET CONTROLLER call ACTIVE JOB NotifyUsersJob.perform_later(@micropost.id) after micropost create

--- apps/service/app/controllers/api/microposts_controller.rb
+++ apps/service/app/controllers/api/microposts_controller.rb
@@ -1,25 +1,32 @@
 class Api::MicropostsController < Api::ApiController
-  before_action :authenticate!, except: %i[create]
-  before_action :correct_user,   only: :destroy
+  before_action :authenticate!
+  before_action :correct_user, only: :destroy
+
   def create
     @micropost = Micropost.new(micropost_params)
-    @micropost.user_id = current_user.id if current_user.present?
+    @micropost.user_id = current_user.id
+
     if @micropost.save
-      render json: { flash: ["success", "Micropost created!"] }
+      NotifyUsersJob.perform_later(@micropost.id)
+      render json: { flash: [ "success", "Micropost created!" ] }
     else
       render json: { error: @micropost.errors.full_messages }
     end
   end
+
   def destroy
     @micropost.destroy
-    render json: { flash: ["success", "Micropost deleted"] }
+    render json: { flash: [ "success", "Micropost deleted" ] }
   end
+
   private
-    def micropost_params
-      params.require(:micropost).permit(:content) # :title, :youtube_id
-    end
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      render json: { flash: ["success", "Micropost with id = #{params[:id]} not found!"] } if @micropost.nil?
-    end
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :title, :youtube_id)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    render json: { error: [ "Micropost not found" ] }, status: :not_found unless @micropost
+  end
 end
