fd708465c6a80b9d1e2bde38f1bf66d7363f7c3f
maearon <manhng132@gmail.com>
2026-04-19 15:51:41 +0700

    WEBSOCKET ACTIVE JOB call ACTION CABLE to broadcast video_notifications

--- apps/service/app/jobs/notify_users_job.rb
+++ apps/service/app/jobs/notify_users_job.rb
@@ -1,7 +1,20 @@
 class NotifyUsersJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(micropost_id)
+    micropost = Micropost.includes(:user).find_by(id: micropost_id)
+    return if micropost.blank? || micropost.user.blank?
+
+    ActionCable.server.broadcast(
+      "video_notifications",
+      {
+        type: "VIDEO_SHARED",
+        micropost_id: micropost.id,
+        video_title: micropost.title,
+        sharer_name: micropost.user.name,
+        sharer_id: micropost.user_id,
+        youtube_id: micropost.youtube_id
+      }
+    )
   end
 end
